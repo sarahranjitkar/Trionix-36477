107adce791831cbaa8f79b98d165cb9af4118acc
sarahranjitkar <ranjitkarsarah@gmail>
2026-08-09 16:44:48 -0400

    basic drivetrain code, probably going to change
    (ALL HAIL BROGAN M. PRATT FOR THE TUTORIAL)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class Drivetrain {
+    private DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+    private IMU imu;
+
+    public void init(HardwareMap hwMap) {
+        // Define drive motors
+        frontLeftMotor = hwMap.get(DcMotor.class, "Front Left Motor");
+        frontRightMotor = hwMap.get(DcMotor.class, "Front Right Motor");
+        backLeftMotor = hwMap.get(DcMotor.class, "Back Left Motor");
+        backRightMotor = hwMap.get(DcMotor.class, "Back Right Motor");
+
+        // Set drive motor directions
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set whether drive motors are using encoders
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Define IMU
+        imu = hwMap.get(IMU.class, "imu");
+
+        // Set Robot Orientation
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,   //Placeholder
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD     //Placeholder
+        );
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        // Set Motor Power
+        double frontLeftPower = forward + strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backLeftPower = forward - strafe + rotate;
+        double backRightPower = forward + strafe - rotate;
+
+        // Set Constraints
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        // Set motor max powers
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower/maxPower));
+        frontRightMotor.setPower(maxSpeed * (frontRightPower/maxPower));
+        backLeftMotor.setPower(maxSpeed * (backLeftPower/maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower/maxPower));
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate) {
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta - imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
+    }
+}
